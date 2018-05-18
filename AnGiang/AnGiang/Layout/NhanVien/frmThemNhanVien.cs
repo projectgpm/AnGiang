0cc994b046b8c2e0266d850a2ec994ef73a4f6de
nttamGPM <tamnt@gpm.vn>
2018-05-18 16:51:54 +0700

    frmThemNV

--- AnGiang/AnGiang/Layout/NhanVien/frmThemNhanVien.cs
+++ AnGiang/AnGiang/Layout/NhanVien/frmThemNhanVien.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Drawing;
 using System.Text;
@@ -8,14 +9,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using DevExpress.XtraBars;
 
 namespace AnGiang.Layout.NhanVien
 {
-    public partial class frmThemNhanVien : DevExpress.XtraEditors.XtraForm
+    public partial class frmThemNhanVien : DevExpress.XtraBars.Ribbon.RibbonForm
     {
         public frmThemNhanVien()
         {
             InitializeComponent();
+
+            //BindingList<Customer> dataSource = GetDataSource();
+            //gridControl.DataSource = dataSource;
+            //bsiRecordsCount.Caption = "RECORDS : " + dataSource.Count;
+        }
+        void bbiPrintPreview_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            //gridControl.ShowRibbonPrintPreview();
         }
     }
 }
